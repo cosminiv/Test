c921b137a082acaa3c867388a792a4f8bc08b38b
YRZUR\ivanc <cosmin.ivan@yr-group.ch>
2019-03-12 13:50:27 +0100

    renamed variable

--- ConsoleApp1/Graph/js/epa.js
+++ ConsoleApp1/Graph/js/epa.js
@@ -1,12 +1,11 @@
 jQuery(document).ready(function() {
   var epaDataRecords = JSON.parse(window.epadata);
 
-    console.log(epaDataRecords[0].host);
-
     makeReqPerMinuteChart(epaDataRecords);
 });
 
 function makeReqPerMinuteChart(epaDataRecords) {
+    // Build data
     var minDay = 29,
         minHour = 23,
         minMinute = 53;
@@ -14,20 +13,21 @@ function makeReqPerMinuteChart(epaDataRecords) {
     var minutesInFirstDay = 60 - minMinute;
 
     var labels = [];
-    var data = [];
+    var requestCount = [];
 
     for (var i = 0; i < epaDataRecords.length; i++) {
         var d = epaDataRecords[i].datetime;
         var minuteOrder = (d.day === minDay) ? (d.minute - minMinute) : (minutesInFirstDay + d.hour * 60 + d.minute);
 
-        if (typeof data[minuteOrder] === 'undefined')
-            data[minuteOrder] = 0;
+        if (typeof requestCount[minuteOrder] === 'undefined')
+            requestCount[minuteOrder] = 0;
         else
-            data[minuteOrder]++;
+            requestCount[minuteOrder]++;
 
         labels[minuteOrder] = d.day + ' Aug ' + d.hour + ':' + d.minute;
     }
 
+    // This uses chart.js (https://www.chartjs.org)
     var ctx = document.getElementById('requestsPerMinuteChart').getContext('2d');
     var chart = new Chart(ctx, {
         type: 'line',
@@ -35,7 +35,7 @@ function makeReqPerMinuteChart(epaDataRecords) {
             labels: labels,
             datasets: [{
                 borderColor: 'rgb(255, 99, 132)',
-                data: data,
+                data: requestCount,
             }]
         },
 
